562e5b6310d010bf6e2a09dcfc45a2c50ef2c778
Uhembe Nelson <nelson.uhembe@peerpay.ng>
2022-06-22 11:10:29 +0100

    updated Assets folder

---
+++ Src/Screens/Afcs/Poc.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+
+const Poc =()=>{
+
+
+    return(
+        <View>
+            <Text>Pay on Card</Text>
+        </View>
+    )
+}
+
+export default Poc;
