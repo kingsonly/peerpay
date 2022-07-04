5e2188a23cd6b36c4a5945ca61fef5bfe971af64
sunday sayil <sundaysayil4u@gmail.com>
2022-07-04 22:51:51 +0100

    make i agree togglr functional

--- __tests__/components/Widgets/widgets.test.js
+++ __tests__/components/Widgets/widgets.test.js
@@ -1,7 +1,9 @@
 import 'react-native';
 import React from 'react';
 import * as widgets from "../../../Src/Components/Widgets/Index"
-import renderer from 'react-test-renderer';
+import Signup from "../../../Src/Screens/Auth/Signup"
+import renderer, { act } from 'react-test-renderer';
+
 
 it('renders correctly', () => {
     const component = renderer.create(
@@ -131,11 +133,58 @@ describe("Test Trans Card Component", () => {
 describe("Test Requests Card Component", () => {
     test("renders Requests Card Component", () => {
         const component = renderer.create(
-            <widgets.TransCard></widgets.TransCard>
+            <widgets.RequestCard></widgets.RequestCard>
         )
     })
 })
 
+
+
+describe("Test Signup Screen", () => {
+    const signUpWidget = renderer.create(<Signup></Signup>)
+    it("should Render Test Signup Screen", () => {
+        signUpWidget.toJSON()
+        expect(signUpWidget).toBeTruthy()
+    })
+
+
+    it('should handle onChange event on name', () => {
+        const testInstance = signUpWidget.root;
+        const mEvent = { target: { value: 'teresa teng' } };
+        act(() => {
+            testInstance.findByProps({ testID: 'name' }).props.onChangeText(mEvent);
+        });
+        expect(testInstance.findByProps({ testID: 'name' }).props.value).toEqual('teresa teng');
+    });
+    it('should handle onChange event on email', () => {
+        const testInstance = signUpWidget.root;
+        const mEvent = { target: { value: 'sayil@email' } };
+        const element = testInstance.findByProps({ testID: 'email' })
+        act(() => {
+            element.props.onChangeText(mEvent);
+        });
+        expect(element.props.value).toEqual('sayil@email');
+    });
+
+    it('should handle onChange event on password', () => {
+        const testInstance = signUpWidget.root;
+        const mEvent = { target: { value: 'password' } };
+        const element = testInstance.findByProps({ testID: 'password' })
+        act(() => {
+            element.props.onChangeText(mEvent);
+        });
+        expect(element.props.value).toEqual('password');
+    });
+    it('should handle onChange event on mobile', () => {
+        const testInstance = signUpWidget.root;
+        const mEvent = { target: { value: '09030031914' } };
+        const element = testInstance.findByProps({ testID: 'mobile' })
+        act(() => {
+            element.props.onChangeText(mEvent);
+        });
+        expect(element.props.value).toEqual('09030031914');
+    });
+})
 
 
 
