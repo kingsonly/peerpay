85e2ce2e7e90e9723ec29e0651d422c3914ea2f6
sunday sayil <sundaysayil4u@gmail.com>
2022-07-06 21:18:09 +0100

    fix conflicts

--- __tests__/components/Screens/Auth/Login.test.js
+++ __tests__/components/Screens/Auth/Login.test.js
@@ -5,9 +5,9 @@ import renderer, { act } from 'react-test-renderer';
 
 
 describe ("test login component", ()=>{
-    const signUpWidget = renderer.create(<Login></Login>)
-    it('should handle onChange event on password', () => {
-        const testInstance = signUpWidget.root;
+    const loginUpWidget = renderer.create(<Login></Login>)
+    it('should handle onChange event on password field', () => {
+        const testInstance = loginUpWidget.root;
         const mEvent = { target: { value: 'password' } };
         const element = testInstance.findByProps({ testID: 'password' })
         act(() => {
@@ -16,4 +16,5 @@ describe ("test login component", ()=>{
         expect(element.props.value).toEqual('password');
     });
 })
+
 
